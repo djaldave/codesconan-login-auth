dd9c7639291632ffefa3469e44939113602112ef
laevad <77134175+laevad@users.noreply.github.com>
2023-07-16 03:37:06 +0800

    done

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
     <div className={"flex flex-col items-center mt-4"}>
         <h1 className={"text-4xl font-bold"}>Welcome to Codes Conan</h1>
         <p className={"text-xl"}>This is a Next.js app with NextAuth.js authentication</p>
+
     </div>
   )
 }
